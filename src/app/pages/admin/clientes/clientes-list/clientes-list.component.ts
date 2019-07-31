4ba798e97bfc2f104afd758d002a04d696505454
AlisonPrietoTello <aliprietot@gmail.com>
2019-07-31 08:12:41 -0500

    actualizar de usuarios

--- src/app/pages/admin/clientes/clientes-list/clientes-list.component.ts
+++ src/app/pages/admin/clientes/clientes-list/clientes-list.component.ts
@@ -15,8 +15,8 @@ export class ClientesListComponent implements OnInit {
   public cols: any[];
   public estado:boolean;
   public nuevoC:any;
+  public estadoCliente:any;
   public actualizar:any;
-
   @Input()  
   public nombre:any;
   @Input()
@@ -68,7 +68,7 @@ export class ClientesListComponent implements OnInit {
       "telefonoFijo":this.telefonoFijo,
       "telefonoCelular":this.telefonoCelular,
       "atencion":this.atencion,
-      "estado": this.estado
+      "estado": this.estadoCliente
       } 
       this._AppService.post('clientes/new', this.nuevoC).subscribe(
         result=>{ alert('El cliente se agregado con exito'), 
@@ -89,8 +89,8 @@ export class ClientesListComponent implements OnInit {
      this.atencion=null;
    }
  //TRAER DATOS POR ID
-   public getCliente(cliente: any){
-    console.log(cliente);
+   public setCliente(cliente: any){
+     console.log(cliente)
     this.idCliente=cliente.idCliente;
     this.nombre=cliente.nombre;
     this.nombrecorto=cliente.nombrecorto;
@@ -101,7 +101,7 @@ export class ClientesListComponent implements OnInit {
     this.email=cliente.email;
     this.direccion=cliente.direccion;
     this.documento=cliente.documento;
-    this.estado=cliente.estado;
+    this.estadoCliente=cliente.estado;
   }
   //METODO DE ACTUALIZAR
   public editarDatos(){
@@ -116,7 +116,7 @@ export class ClientesListComponent implements OnInit {
       "telefonoFijo":this.telefonoFijo,
       "telefonoCelular":this.telefonoCelular,
       "atencion":this.atencion,
-      "estado": this.estado
+      "estado": this.estadoCliente
     }
     this._AppService.put('cliente/'+this.idCliente,this.actualizar).subscribe(
       result=>{
@@ -127,13 +127,9 @@ export class ClientesListComponent implements OnInit {
       } 
     )
   }
-  //CAMBIAR EL ESTADO
-
   ngOnInit() {
     this.getTerceros();
   }
- 
-
   //GET TERCEROS
   public getTerceros(){
     this._AppService.get(`clientes/list`).subscribe(
@@ -156,12 +152,7 @@ export class ClientesListComponent implements OnInit {
       type: 'success',
       title: 'El registro a sido exitoso!'
     })
-
-
-
   }
-
-
   public deleteCliente(){
     const clienteJson = {
       "idCliente":this.idCliente,
@@ -186,6 +177,4 @@ export class ClientesListComponent implements OnInit {
       }
     )
   }
-
-
 }
--- src/app/pages/admin/instrumentos/instrumentos-list/instrumentos-list.component.ts
+++ src/app/pages/admin/instrumentos/instrumentos-list/instrumentos-list.component.ts
@@ -3,6 +3,10 @@ import { AppSettings } from '../../../../app.settings';
 import { Settings } from '../../../../app.settings.model';
 import { AppService } from 'src/app/services/app.service';
 import Swal from 'sweetalert2';
+import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from 'src/app/services/auth.service';
+
+
 @Component({
   selector: 'app-instrumentos-list',
   templateUrl: './instrumentos-list.component.html',
@@ -21,6 +25,9 @@ export class InstrumentosListComponent implements OnInit {
   public selectinstrumento:any;
   public empresa: 1;
   public vacion:boolean;
+  public actualizar:any;
+  public usuario:Usuario;
+  public idInstrumentos:any;
   //INPUT 
   @Input()
   public nombre:any;
@@ -39,11 +46,10 @@ export class InstrumentosListComponent implements OnInit {
   });
   limls: { field: string; header: string; }[];
   constructor(public appSettings:AppSettings,
-    private _AppService:AppService) { this.settings = this.appSettings.settings
+    private _AppService:AppService, public servicio:AuthService) { this.settings = this.appSettings.settings
     this.estado='listaI';
     this.vacion=false;
     this.lista='instrumento';
-
     this.cols = [
       { field: 'idInstrumentos', header: 'id' },
       { field: 'nombre', header: 'Nombre' },
@@ -52,10 +58,7 @@ export class InstrumentosListComponent implements OnInit {
       { field: 'accion', header: 'acccion' }
     ];  }
 
-  ngOnInit() {
-    this.getInstrumentos();
-    this.getProtocolos();
-  }
+  //obetener el selecionado 
   SelectIntrumento(selec:any){
     this.selectinstrumento=selec;
     console.log(this.selectinstrumento)
@@ -63,7 +66,7 @@ export class InstrumentosListComponent implements OnInit {
    // @Get protocolos por Instrumentos
    public getProtocolosPorInstrumento(e){
      this.protocolos = [];
-    this._AppService.get('ip/1/instrumento/'.concat(e)).subscribe(
+    this._AppService.get('ip/'+this.usuario.empresa.idEmpresa+'/instrumento/'.concat(e)).subscribe(
       (result:any) => {
         this.protocolos = result;
         if(this.protocolos.length > 0){this.table = 1;}
@@ -82,6 +85,27 @@ export class InstrumentosListComponent implements OnInit {
           console.log(error);
         });
     }
+    //OBTENER ID
+    public SeleInstrumento(instrumento:any){
+      this.idInstrumentos.instrumento.idInstrumento;
+      this.nombre=instrumento.nombre;
+      this.modelo=instrumento.modelo;
+      this.marca=instrumento.marca;
+    }
+    // ACTUALIZAR INSTRUMENTOS
+    public putInstrumento(){
+      this.actualizar={
+        "idInstrumentos":this.idInstrumentos,
+        "nombre":this.nombre,
+        "marca":this.marca,
+        "modelo":this.modelo,
+      }
+      console.log(this.actualizar)
+      this._AppService.put('instrumentos/'+this.idInstrumentos,this.actualizar).subscribe(
+        result=>{
+          console.log(result)}
+      )
+    }
     //AGREGAR NUEVO INSTRUMENTO
     public postInstrumentos(){
       this.instrumento={
@@ -92,16 +116,16 @@ export class InstrumentosListComponent implements OnInit {
       }
       console.log(this.instrumento)
       this._AppService.post('instrumentos/new', this.instrumento).subscribe(
-        result=>{  this.success
+        result=>{ Swal.fire('login','El instrumento se agregado con exito')
           this.instrumento=result
-          this.getInstrumentos();
           this.estado='listaI';
+          this.getInstrumentos();
+          
         }
       )
     }
     //AGREGAR NUEVO INSTRUMENTO PROTOCOLO 
     public postIntrumentosProtocolo(){
-
       this.instrumento={
         "fkProtocolo":this.proto,
         "fkIntrumento":this.selectinstrumento
@@ -120,18 +144,10 @@ export class InstrumentosListComponent implements OnInit {
         this.Toast.fire({type: 'error',title: 'Ha ocurrido un error en la consulta'})
       });
   }
-  success(){
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000
-    });
-    
-    Toast.fire({
-      type: 'success',
-      title: 'El instrumento fue agregado con exito'
-    })
+  ngOnInit() {
+    this.getInstrumentos();
+    this.getProtocolos();
+    this.usuario=this.servicio.obtenerDatosUser();
   }
 
 }
